4c144cf34f78ef01ea705ba936c7c5d7e75628a9
drownedout <danielcdonnelly@gmail.com>
2016-08-12 08:04:19 -0400

    Added relation to User and Post, tests

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -14,17 +14,28 @@ describe 'navigate' do
 	end
 
 	describe 'creation' do
-		it 'new form that can reached' do
+		before do
+			user = User.create(email: "test@test.com", password: "password", password_confirmation: "password", first_name: "Sally", last_name: "Sample")
+			login_as(user, :scope => :user)
 			visit new_post_path
+		end
+
+		it 'new form that can reached' do
 			expect(page.status_code).to eq(200)
 		end
 
 		it 'can be created from new form page' do
-			visit new_post_path
 			fill_in 'post[date]', with: Date.today
 			fill_in 'post[rationale]', with: "Some rationale"
 			click_on "Save"
 			expect(page).to have_content("Some rationale")
+		end
+
+		it 'will have a user associate with a post' do
+			fill_in 'post[date]', with: Date.today
+			fill_in 'post[rationale]', with: "User Association"
+			click_on "Save"
+			expect(User.last.posts.last.rationale).to eq("User Association")
 		end
 	end
 end
