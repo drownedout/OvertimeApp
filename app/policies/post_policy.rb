65972c46abab727415ac87216604b02097be2d75
drownedout <danielcdonnelly@gmail.com>
2016-08-21 17:42:59 -0400

    Added Post policy

---
+++ app/policies/post_policy.rb
@@ -0,0 +1,11 @@
+class PostPolicy < ApplicationPolicy
+
+	def update?
+		#Case 1 = User of record
+		record.user_id == user.id || admin_types.include?(user.type)
+
+		#Case 2 = Admin user
+
+	end
+
+end
