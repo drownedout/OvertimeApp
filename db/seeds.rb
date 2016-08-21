98ad87105a1a12f34d3b4f654d3bbb898d4117a6
drownedout <danielcdonnelly@gmail.com>
2016-08-21 16:21:51 -0400

    Added edit functionality and tests for post status

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
 @user = User.create(email: "test@test.com", password: "password", password_confirmation: "password", first_name: "John", last_name: "Example")
 puts "User created"
 
+AdminUser.create(email: "test1@test.com", password: "password", password_confirmation: "password", first_name: "Admin", last_name: "Example")
+puts "AdminUser created"
+
 100.times do |post|
 	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
