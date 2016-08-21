abf886128be8a6cf8bcad8e6b17fb795dd1610e1
drownedout <danielcdonnelly@gmail.com>
2016-08-20 20:40:01 -0400

    Updated Admin Dashboard view

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -36,7 +36,7 @@ class UserDashboard < Administrate::BaseDashboard
     :posts,
     :id,
     :email,
-    :encrypted_password,
+    :type
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
@@ -45,40 +45,18 @@ class UserDashboard < Administrate::BaseDashboard
     :posts,
     :id,
     :email,
-    :encrypted_password,
-    :reset_password_token,
-    :reset_password_sent_at,
-    :remember_created_at,
-    :sign_in_count,
-    :current_sign_in_at,
-    :last_sign_in_at,
-    :current_sign_in_ip,
-    :last_sign_in_ip,
     :first_name,
     :last_name,
-    :type,
-    :created_at,
-    :updated_at,
+    :type
   ].freeze
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
-    :posts,
     :email,
-    :encrypted_password,
-    :reset_password_token,
-    :reset_password_sent_at,
-    :remember_created_at,
-    :sign_in_count,
-    :current_sign_in_at,
-    :last_sign_in_at,
-    :current_sign_in_ip,
-    :last_sign_in_ip,
     :first_name,
     :last_name,
-    :type,
   ].freeze
 
   # Overwrite this method to customize how users are displayed
