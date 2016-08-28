ce08063208236298a8ef7d282dfb228c0c045c59
drownedout <danielcdonnelly@gmail.com>
2016-08-28 16:13:43 -0400

    Included application helper for status labels

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,19 +1,3 @@
 module PostsHelper
 
-	def status_label status
-		status_label_generator status
-	end
-
-	private
-
-	def status_label_generator status
-		case status
-		when 'submitted'
-			content_tag(:span, status.titleize, class: 'label label-primary')
-		when 'approved'
-			content_tag(:span, status.titleize, class: 'label label-success')
-		when 'rejected'
-			content_tag(:span, status.titleize, class: 'label label-danger')
-		end
-	end
 end
