6f20ef54351eba23142ffc66d817e0f8b3d1a954
drownedout <danielcdonnelly@gmail.com>
2016-08-30 18:38:16 -0400

    Setup basic employee homepage, refactored controller for homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
-  include Pundit	
-  protect_from_forgery with: :exception
-  before_action :authenticate_user!
+	include Pundit	
+	protect_from_forgery with: :exception
+	before_action :authenticate_user!
 
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+	rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-  private
+	def admin_types
+		['AdminUser']
+	end
+	
+	private
 	def user_not_authorized
 		flash[:alert] = "You are not authorized to view this page"
 		redirect_to(request.referrer || root_path)
