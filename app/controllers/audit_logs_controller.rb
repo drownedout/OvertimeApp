68ce5fd69fe8b8a213b3ac5935417a418a40cd5d
drownedout <danielcdonnelly@gmail.com>
2016-08-30 20:19:27 -0400

    Added functional buttons for employee confirmations, tests

--- app/controllers/audit_logs_controller.rb
+++ app/controllers/audit_logs_controller.rb
@@ -3,4 +3,14 @@ class AuditLogsController < ApplicationController
 		@audit_logs = AuditLog.page(params[:page]).per(10)
 		authorize @audit_logs
 	end
+
+	def confirm
+		@audit_log = AuditLog.find(params[:id])
+
+		authorize @audit_log
+
+		@audit_log.confirmed!
+
+		redirect_to root_path, notice: "You have confirmed your overtime"
+	end
 end
